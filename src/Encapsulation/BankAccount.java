e611079a2e10d8b88c2f9389805b6a4e843ce426
Abhinav5021 <abhinavspawar2005@gmail.com>
2026-07-22 13:38:45 +0530

    Encapsulation Questions solved

--- src/Encapsulation/BankAccount.java
+++ src/Encapsulation/BankAccount.java
@@ -2,4 +2,25 @@ package Encapsulation;
 
 public class BankAccount {
 
+	private double balance = 5000;
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public double deposit(double amount) {
+		balance+=amount;
+		return balance;
+	}
+	public double withdrawal(double amount) {
+		balance-=amount;
+		return balance;
+	}
+
+	
+
 }
--- src/Encapsulation/Main.java
+++ src/Encapsulation/Main.java
@@ -16,15 +16,75 @@ public class Main {
 			System.out.println("2. Bank Account");
 			System.out.print("Enter your Choice : ");
 			choice = sc.nextInt();
+			sc.nextLine();
 			switch (choice) {
 			case 1: {
-				System.out.println("Person");
+				System.out.println("Person : ");
+				Person p = new Person();
+				System.out.println("Enter Person Name : ");
+				String name = sc.nextLine();
+				p.setName(name);
+				System.out.println("Enter Person Age : ");
+				p.setAge(sc.nextInt());
+
+				System.out.println("Name is : " + p.getName() + " and Age is : " + p.getAge());
+
 				break;
 			}
 			case 2: {
-				System.out.println("Bank Account");
+				BankAccount ba = new BankAccount();
+				System.out.println("Bank Account : ");
+				System.out.println("Enter Initial Balance : ");
+				ba.setBalance(sc.nextInt());
+				int ch = 1;
+				while (ch != 0) {
+					System.out.println("1. Check Balance");
+					System.out.println("2. Deposit");
+					System.out.println("3. Withdrawal");
+					System.out.println("0. Exit");
+					ch = sc.nextInt();
+
+					switch (ch) {
+					case 1: {
+						System.out.println("Your Current Balance is : " + ba.getBalance());
+						break;
+					}
+					case 2: {
+						System.out.print("Enter Amount to Deposit : ");
+						System.out.println("Deposit Success.. Updated balance is : "+ba.deposit(sc.nextDouble()));
+						break;
+					}
+					case 3: {
+						System.out.print("Enter Amount to Withdraw : ");
+						double amount = sc.nextDouble();
+						if (amount > ba.getBalance() && amount > 0) {
+							System.err.println("Insufficient Funds .. !!");
+						} else {
+							System.out.println("Withdrawal Amount is : " + amount);
+							System.out
+									.println("Withdrawal Successfull..  Updated Balance is : " + ba.withdrawal(amount));
+						}
+						break;
+
+					}
+					case 0:{
+						System.out.println("Exited Successfully.. !!");
+					}
+					default:{
+						System.err.println("Invalid Input .. !!");
+					}
+
+					}
+				}
 				break;
 			}
+			case 0: {
+				System.out.println("Exited Successfully .. !!");
+			}
+			default: {
+				System.err.println("Invalid Choice .. !!");
+			}
+
 			}
 
 		}
